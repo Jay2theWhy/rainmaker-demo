6079adb17c259657f3f422973bae81127a530313
unknown <jyfan16@hotmail.com>
2024-09-22 19:49:12 -0400

    style and align sliders and options

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,72 @@
-import React from 'react';
-import { useState } from 'react';
-import './App.css';
-import { RainEffect } from 'rainmaker-react';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import { RainEffect } from "rainmaker-react";
 
 const App: React.FC = () => {
-  const [count, setCount] = useState<number>(50);
+  const [count, setCount] = useState<number>(800);
   const [wind, setWind] = useState<number>(0);
-
-  const handleCountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = parseInt(event.target.value, 10);
-    setCount(value);
+  const min = {
+    count: 0,
+    wind: -20,
+  };
+  const max = {
+    count: 2000,
+    wind: 20,
   };
 
-  const handleWindChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = parseInt(event.target.value, 0);
-    setWind(value);
+  const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(event.target.value, 10);
+    switch (event.target.name) {
+      case "count": {
+        setCount(value);
+        break;
+      }
+      case "wind": {
+        setWind(value);
+        break;
+      }
+    }
   };
 
   return (
     <div>
       <div className="title">RAINMAKER DEMO</div>
-      <div className="slider-container">
-        <input 
-          type="range"
-          value={count}
-          onChange={handleCountChange}
-          min="0"
-          max="1000"
-        />
-        <span className="count-display">{count}</span>
-        <input 
-          type="range"
-          value={wind}
-          onChange={handleWindChange}
-          min="-10"
-          max="10"
-        />
-        <span className="count-display">{wind}</span>
+      <div className="options-container">
+        <div className="option-entry">
+          <span className="option-name">Intensity: {count}</span>
+          <div className="option-slider">
+            <span className="slider-label">{min["count"]}</span>
+            <input
+              type="range"
+              name="count"
+              value={count}
+              onChange={handleSliderChange}
+              min={min["count"]}
+              max={max["count"]}
+            />
+            <span className="slider-label">{max["count"]}</span>
+          </div>
+        </div>
+        <div className="option-entry">
+          <span className="option-name">Wind: {wind}</span>
+          <div className="option-slider">
+            <span className="slider-label">{min["wind"]}</span>
+            <input
+              type="range"
+              name="wind"
+              value={wind}
+              onChange={handleSliderChange}
+              min={min["wind"]}
+              max={max["wind"]}
+            />
+            <span className="slider-label">{max["wind"]}</span>
+          </div>
+        </div>
       </div>
-      <RainEffect className="rain-effect" count={count} wind={wind}/>
+      <RainEffect className="rain-effect" count={count} wind={wind} />
     </div>
   );
-}
+};
 
 export default App;
